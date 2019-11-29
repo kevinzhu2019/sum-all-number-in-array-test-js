f6435af0147f94ec35ef72c52d93c3f935505c21
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-11-29 21:26:34 +0000

    commit the summary array recursion function

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  if (Array.isArray(array)) {
+    let sum = 0
+    for (const item of array) {
+      if (Array.isArray(item)) {
+        sum = sum + sumItems(item);
+      } else {
+        sum = sum + item;
+      }
+    };
+    return sum;
+  }
 }
 
 module.exports = sumItems;
